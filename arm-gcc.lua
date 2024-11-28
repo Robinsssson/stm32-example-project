84d762930edcf4accd667c2237d59655e0473b29
robinson <18656133516@gmail.com>
2024-11-28 17:37:44 +0800

    fix: change format

--- arm-gcc.lua
+++ arm-gcc.lua
@@ -16,20 +16,21 @@ toolchain("arm-gcc")
             toolchain:set("toolset", "as", "arm-none-eabi-gcc")
         end
     end)
+
 toolchain_end()
 
 rule("generate-hex")
     after_build(function(target)
-        print("$(env ARM_TOOL)")
-        print("after_build: generate hex files")
         local out = target:targetfile() or ""
         local gen_fi = "build/" .. target:name()
         print(string.format("%s => %s", out, gen_fi))
+
         -- https://github.com/xmake-io/xmake/discussions/2125
         -- os.exec("arm-none-eabi-objdump -S "..out.." > "..gen_fi..".asm")
         -- local asm = os.iorun("arm-none-eabi-objdump -S build/cross/cortex-m4/release/minimal-proj")
         -- io.writefile(gen_fi..".asm", asm)
-        if is_plat("windows") then
+
+        if target:is_plat("windows") then
             os.execv("arm-none-eabi-objcopy.exe", {"-Obinary", out, gen_fi .. ".bin"})
             os.execv("arm-none-eabi-objdump.exe", {"-S", out}, {stdout = gen_fi .. ".asm"})
             os.execv("arm-none-eabi-objcopy.exe", {"-O", "ihex", out, gen_fi .. ".hex"})
@@ -38,19 +39,23 @@ rule("generate-hex")
             os.execv("arm-none-eabi-objdump", {"-S", out}, {stdout = gen_fi .. ".asm"})
             os.execv("arm-none-eabi-objcopy", {"-O", "ihex", out, gen_fi .. ".hex"})
         end
+
         os.mv(out, gen_fi .. ".elf") -- add .elf file
         --  -I binary
         -- $(Q) $(OBJ_COPY) -O ihex $@ $(BUILD_DIR)/$(TARGET).hex
         -- $(Q) $(OBJ_COPY) -O binary $@ $(BUILD_DIR)/$(TARGET).bin
         -- os.exec("qemu-system-arm -M stm32-p103 -nographic -kernel"..bin_out)
     end)
+
     after_clean(function(target)
         local gen_fi = "build/" .. target:name()
         os.rm(gen_fi .. ".*")
     end)
+
 rule_end()
 
 task("qemu")
+
     on_run(function()
         print("Run binary in Qemu!")
         local bin_out = os.files("$(buildir)/*.bin")[1]
@@ -60,6 +65,7 @@ task("qemu")
             print("Do not find bin file in $(buildir)/")
         end
     end)
+
     set_menu {
         -- Settings menu usage
         usage = "xmake qemu",
@@ -67,4 +73,5 @@ task("qemu")
         -- Setup menu description
         description = "Run binary in Qemu!"
     }
+    
 task_end()
